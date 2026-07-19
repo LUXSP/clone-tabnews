f11f2796827130556a624cdc8cec365b31a6c6d7
Ryan Tavares Ternaveski Lopes <141194635+LUXSP@users.noreply.github.com>
2026-07-19 23:57:39 +0000

    adiciona scripts `lint:check` e `lint:fix` no package.json

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto feito mediante ao curso.dev de Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-function Home () {
-    return (
-        <>
-            <h1>Eu te amo, Júlia Maria!</h1>
-            <h3>
-            💞💗💖 Quero que saiba o quão você é uma pessoa incrível,
-            o quão você melhora significativamente tudo. 💞💗💖
-            </h3>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <h1>Eu te amo, Júlia Maria!</h1>
+      <h3>
+        💞💗💖 Quero que saiba o quão você é uma pessoa incrível, o quão você
+        melhora significativamente tudo. 💞💗💖
+      </h3>
+    </>
+  );
 }
 
 export default Home;
